ea71c046acdd05cdafbfdb898228077be18595d2
Ken Suon <ken.su_dev@hotmail.com>
2021-10-17 19:49:05 +0200

    Login works

--- app/src/main/java/com/suonk/whatsapp_clone/ui/fragments/login/LoginFragment.kt
+++ app/src/main/java/com/suonk/whatsapp_clone/ui/fragments/login/LoginFragment.kt
@@ -1,27 +1,50 @@
 package com.suonk.whatsapp_clone.ui.fragments.login
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.res.ResourcesCompat
-import com.google.android.material.tabs.TabLayoutMediator
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatImageView
+import com.google.firebase.auth.FirebaseAuth
 import com.suonk.whatsapp_clone.R
 import com.suonk.whatsapp_clone.databinding.FragmentLoginBinding
-import com.suonk.whatsapp_clone.databinding.FragmentMainLoginBinding
 import com.suonk.whatsapp_clone.ui.activity.MainActivity
-import com.suonk.whatsapp_clone.ui.adapters.LoginViewPagerAdapter
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
+import java.lang.Exception
+import java.util.regex.Pattern
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class LoginFragment : Fragment() {
 
-    private var binding: FragmentMainLoginBinding? = null
+    //region ========================================== Val or Var ==========================================
+
+    private var binding: FragmentLoginBinding? = null
+    private lateinit var cA: MainActivity
+
+    @Inject
+    lateinit var auth: FirebaseAuth
+    private var passwordVisible = false
+
+    //endregion
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMainLoginBinding.inflate(inflater, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
         initializeUI()
         return binding?.root
     }
@@ -29,43 +52,163 @@ class LoginFragment : Fragment() {
     //region ============================================== UI ==============================================
 
     private fun initializeUI() {
-        setupViewPager()
+        cA = activity as MainActivity
+        signUpEmailTextListener()
+        loginPasswordChangeVisibilityClick()
+        loginButtonClick()
     }
 
-    private fun setupViewPager() {
-        binding?.loginViewPager?.adapter = LoginViewPagerAdapter(activity as MainActivity)
+    //endregion
 
-        if (binding?.loginTabLayout != null && binding?.loginViewPager != null) {
-            TabLayoutMediator(
-                binding?.loginTabLayout!!,
-                binding?.loginViewPager!!
-            ) { tab, position ->
-                when (position) {
-                    0 -> {
-                        tab.text = "Login"
-//                        tab.icon = ResourcesCompat.getDrawable(resources, R.drawable.ic_notes, null)
-                    }
-                    1 -> {
-                        tab.text = "Sign Up"
-//                        tab.icon = ResourcesCompat.getDrawable(
-//                            resources,
-//                            R.drawable.ic_star_selected,
-//                            null
-//                        )
-                    }
-                }
-            }.attach()
+    //region ============================================ Clicks ============================================
+
+    private fun loginButtonClick() {
+        binding?.logInButton?.setOnClickListener {
+            if (checkAll()) {
+                loginUser()
+            }
+        }
+    }
+
+    private fun loginPasswordChangeVisibilityClick() {
+        binding?.apply {
+            loginPasswordGoToVisible.setOnClickListener {
+                passwordChangeVisibility(loginPasswordGoToVisible, loginPassword)
+            }
+        }
+    }
+
+    private fun passwordChangeVisibility(iV: AppCompatImageView, password: AppCompatEditText) {
+        val invisible = AppCompatResources.getDrawable(cA, R.drawable.password_invisible_animation)
+        val visible = AppCompatResources.getDrawable(cA, R.drawable.password_visible_animation)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val frameAnimation = iV.drawable as AnimationDrawable
+            frameAnimation.start()
+            delay(525)
+            frameAnimation.stop()
+            if (passwordVisible) {
+                iV.setImageDrawable(visible)
+                password.transformationMethod = PasswordTransformationMethod.getInstance()
+                passwordVisible = false
+            } else {
+                iV.setImageDrawable(invisible)
+                password.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                passwordVisible = true
+            }
         }
     }
 
     //endregion
 
-    //region =========================================== LifeCycle ==========================================
+    //region =========================================== Listeners ==========================================
+
+    private fun signUpEmailTextListener() {
+        binding?.loginEmail?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                checkEmailConstantly()
+            }
+        })
+    }
+
+    //endregion
+
+    //region ============================================ Checks ============================================
+
+    private fun checkIfEmptyFields(): Boolean {
+        return binding?.loginEmail?.text?.isNotEmpty()!! ||
+                binding?.loginPassword?.text?.isNotEmpty()!!
+    }
+
+    private fun checkEmailValidationLogin(): Boolean {
+        val emailPattern = Pattern.compile("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")
+        val userEmail = binding?.loginEmail?.text?.toString()
+
+        return userEmail?.trim()?.matches(emailPattern.toRegex())!!
+    }
+
+    private fun checkEmailConstantly() {
+        val emailPattern = Pattern.compile("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")
+        binding?.apply {
+            if (loginEmail.text.toString().trim().matches(emailPattern.toRegex())) {
+                loginEmailValidation.visibility = View.VISIBLE
+                loginEmailValidation.setImageDrawable(
+                    AppCompatResources.getDrawable(
+                        cA,
+                        R.drawable.ic_check_email
+                    )
+                )
+            } else {
+                if (loginEmail.text.toString().isEmpty()) {
+                    loginEmailValidation.visibility = View.INVISIBLE
+                } else {
+                    loginEmailValidation.visibility = View.VISIBLE
+                    loginEmailValidation.setImageDrawable(
+                        AppCompatResources.getDrawable(
+                            cA,
+                            R.drawable.ic_check_email_cross
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    private fun checkAll(): Boolean {
+        return if (checkIfEmptyFields()) {
+            if (checkEmailValidationLogin()) {
+                true
+            } else {
+                toastWarningMessage(getString(R.string.email_not_valid_msg))
+                false
+            }
+        } else {
+            toastWarningMessage(getString(R.string.fields_empty_msg))
+            false
+        }
+    }
+
+    //endregion
+
+    //region ========================================= User Firebase ========================================
+
+    private fun loginUser() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                binding?.apply {
+                    auth.signInWithEmailAndPassword(
+                        loginEmail.text.toString(),
+                        loginPassword.text.toString()
+                    ).await()
+
+                    withContext(Dispatchers.Main) {
+                        toastWarningMessage("Log In")
+                        cA.showOnlineChatFragment()
+                    }
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    toastWarningMessage(e.message!!)
+                }
+            }
+        }
+    }
+
+    //endregion
+
+    private fun toastWarningMessage(msg: String) {
+        Log.i("toastWarningMessage", msg)
+        Toast.makeText(cA, msg, Toast.LENGTH_LONG).show()
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
     }
-
-    //endregion
 }
--- app/src/main/java/com/suonk/whatsapp_clone/ui/fragments/login/MainLoginFragment.kt
+++ app/src/main/java/com/suonk/whatsapp_clone/ui/fragments/login/MainLoginFragment.kt
@@ -1,24 +1,58 @@
 package com.suonk.whatsapp_clone.ui.fragments.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import com.suonk.whatsapp_clone.R
-import com.suonk.whatsapp_clone.databinding.FragmentLoginBinding
+import com.suonk.whatsapp_clone.databinding.FragmentMainLoginBinding
+import com.suonk.whatsapp_clone.ui.activity.MainActivity
+import com.suonk.whatsapp_clone.ui.adapters.LoginViewPagerAdapter
 
 class MainLoginFragment : Fragment() {
 
-    private var binding: FragmentLoginBinding? = null
+    private var binding: FragmentMainLoginBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding = FragmentMainLoginBinding.inflate(inflater, container, false)
+        initializeUI()
         return binding?.root
     }
+
+    //region ============================================== UI ==============================================
+
+    private fun initializeUI() {
+        setupViewPager()
+    }
+
+    private fun setupViewPager() {
+        binding?.loginViewPager?.adapter = LoginViewPagerAdapter(activity as MainActivity)
+
+        if (binding?.loginTabLayout != null && binding?.loginViewPager != null) {
+            TabLayoutMediator(
+                binding?.loginTabLayout!!,
+                binding?.loginViewPager!!
+            ) { tab, position ->
+                when (position) {
+                    0 -> {
+                        tab.icon = ResourcesCompat.getDrawable(resources, R.drawable.ic_login, null)
+                    }
+                    1 -> {
+                        tab.icon =
+                            ResourcesCompat.getDrawable(resources, R.drawable.ic_sign_up, null)
+                    }
+                }
+            }.attach()
+        }
+    }
+
+    //endregion
 
     override fun onDestroyView() {
         super.onDestroyView()
